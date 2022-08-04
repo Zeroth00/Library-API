9d6d46901fea78d0af5b1cc9bc114e2a42d98362
Konrad <konrad.maj@spoko.pl>
2022-08-04 12:22:01 +0200

    HerokuFix

--- RestApiProje/Program.cs
+++ RestApiProje/Program.cs
@@ -19,11 +19,10 @@ builder.Services.AddDbContext<ClientsDbManager>(options => options.UseSqlServer(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseAuthorization();
 
